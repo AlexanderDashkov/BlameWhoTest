5b2bba6df2a65c862eb7c6ddded8bd4b966e131a
DashkovAlexander <Dashkov.Alexander.OM@gmail.com>
2016-02-20 01:01:10 +0300

    added Jack exception method

---
+++ src/SampleClassOne.java
@@ -0,0 +1,11 @@
+/**
+ * Created by александр on 20.02.2016.
+ */
+public class SampleClassOne {
+
+    // throws NullPointer Exception
+    public void invokeJackException(){
+        String s = null;
+        s.hashCode();
+    }
+}
