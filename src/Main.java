1d38458efe7dcf2d37c1bdae7b985e7e39266262
DashkovAlexander <SanchousDas@gmail.com>
2016-02-20 01:11:59 +0300

    added Jane's exception method

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         SampleClassOne sampleClassOne = new SampleClassOne();
         sampleClassOne.invokeJackException();
+		sampleClassOne.invokeJaneException();
     }
 }
