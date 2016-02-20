9c7e1d856926d986ad5e462dff93d94bd8ad1025
DashkovAlexander <Dashkov.Alexander.OM@gmail.com>
2016-02-20 15:17:10 +0300

    added package with exception

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import ru.sample.SampleClassTwo;
+
 /**
  * Created by Alexander on 20.02.2016.
  */
 public class Main {
     public static void main(String[] args) {
         SampleClassOne sampleClassOne = new SampleClassOne();
-        sampleClassOne.invokeJackException();
+        SampleClassTwo sampleClassTwo= new SampleClassTwo();
+        sampleClassTwo.packageExceptionMethod();
+    //    sampleClassOne.invokeJackException();
     }
 }
---
+++ src/ru/sample/SampleClassTwo.java
@@ -0,0 +1,12 @@
+package ru.sample;
+
+/**
+ * Created by Alexander on 20.02.2016.
+ */
+public class SampleClassTwo {
+
+    public void packageExceptionMethod(){
+        throw new RuntimeException();
+    }
+
+}
