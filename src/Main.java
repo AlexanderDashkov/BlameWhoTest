299fc6c8e419ebfb5ac1012ac57999dffe20a165
JackSmithJunior <Dashkov.Alexander.OM@gmail.com>
2016-03-04 18:23:14 +0300

    New types of exception added

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 import ru.sample.SampleClassTwo;
 
+import java.io.IOException;
+
 /**
  * Created by Alexander on 20.02.2016.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SampleClassOne sampleClassOne = new SampleClassOne();
         SampleClassTwo sampleClassTwo = new SampleClassTwo();
-        sampleClassTwo.packageExceptionMethod();
+//        sampleClassTwo.packageExceptionMethod();
         sampleClassOne.invokeJackException();
-		sampleClassOne.invokeJaneException();
+//		sampleClassOne.invokeJaneException();
     }
 }
--- src/SampleClassOne.java
+++ src/SampleClassOne.java
@@ -1,14 +1,15 @@
-/**
- * Created by александр on 20.02.2016.
- */
-public class SampleClassOne {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+public class SampleClassOne {
     // throws NullPointer Exception
-    public void invokeJackException(){
-        String s = null;
-        s.hashCode();
+    public void invokeJackException() throws IOException{
+        Files.createFile(Paths.get("fileToDelete.txt"));
+        Path fileToDeletePath = Paths.get("fileToDelete_jdk7.txt");
+        Files.delete(fileToDeletePath);
     }
-	
 	//throws ArithmeticException
 	public void invokeJaneException(){
 	    int n = 10 / 0; 
